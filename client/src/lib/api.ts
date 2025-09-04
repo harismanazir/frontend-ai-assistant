03e3fc87ddce46d6e653d1301c1743c90bc1058a
Haris Manazir <harismanazir123@gmail.com>
2025-09-04 22:04:07 +0530

    fixed base url

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://experts-represents-antigua-strategy.trycloudflare.com";
+const API_BASE_URL = "https://begins-tray-bases-reuters.trycloudflare.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
